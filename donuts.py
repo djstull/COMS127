50f01ddc093b5d8fcd378931115e3fa019f0556c
DJ Stull <djstull@iastate.edu>
2018-09-06 14:47:47 -0500

    Tasty donut calculations added

--- donuts.py
+++ donuts.py
@@ -1,3 +1,13 @@
+#Get initial amount of donuts
 amount = int(input("Hello sir! How many donuts would you like?: "))
+
+#Determine the amount of dozens an subtract that many donuts from the total
+dozens = amount // 12
+amount = amount - (dozens * 12)
+
 cost = amount * .75
+cost = cost + (dozens * 8)
+
 print ("$", cost)
+print (dozens, "Dozen")
+print (amount, "Singles")
