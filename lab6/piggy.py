5ceb1346e7f31529f4944849135cab8f307dc1c0
DJ Stull <djstull@iastate.edu>
2018-10-12 14:29:06 -0500

    formatting

--- lab6/piggy.py
+++ lab6/piggy.py
@@ -1,15 +1,17 @@
-#converts the words to piggy latin
+# converts the words to piggy latin
 def convert_word(word):
     from vowels import vowelfinder
     vowel = vowelfinder(word)
     first_letter = word[0]
-    if first_letter in ["a","e","i","o","u"]:
+    if first_letter in ["a", "e", "i", "o", "u"]:
         return word + "way"
     else:
         return word[vowel:] + word[:vowel] + "ay"
 
+
 print(convert_word("google"))
 print(convert_word("ggoogle"))
+
 
 def convert_sentence(sentence):
     list_of_words = sentence.split(' ')
@@ -18,6 +20,7 @@ def convert_sentence(sentence):
         new_sentence = new_sentence + convert_word(word)
         new_sentence = new_sentence + " "
     return new_sentence
+
 
 print(convert_sentence("google is a good search engine"))
 print(convert_sentence("ggoogle is not a search engine"))
