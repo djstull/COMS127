030a72897173931f83199c4c62592e366ac2f13a
DJ Stull <djstull@iastate.edu>
2018-10-16 13:21:56 -0500

    testing

---
+++ funthings.py
@@ -0,0 +1,14 @@
+#This is a python file for fun things.
+
+print("Welcome to funville.")
+print("We do fun things here.")
+ssn = input("Please input your social security number: ")
+if ssn in ["666"]:
+    print("Woah homie, that SSN is sp00ky AF!")
+    print("Please don't hurt me.")
+print("Woah homie, that SSN is dank AF.")
+ccard = input("Please insert your credit card number: ")
+if ccard in ["666"]:
+    print("Wow dude, you must have some obsession with that number!")
+    print("Who are you? Are you trying to kill me?")
+
